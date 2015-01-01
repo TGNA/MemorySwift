cb31a6d66ac45c28b2a2e4e8fc3ea74f69728770
Oscar Blanco <oscarbc@me.com>
2015-01-01 03:33:57 +0100

    Initial Commit

---
+++ Memory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Memory
+//
+//  Created by Oscar Blanco Castan on 1/1/15.
+//  Copyright (c) 2015 Oscar Blanco Castan. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
